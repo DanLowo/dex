77d5a6c670dcf3bf5d5c1bc7526031cab5a85fb1
DanLowo <danlowo@gmail.com>
2022-03-15 13:05:52 +0100

    home page & index

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { PageAndSidebarWrapper } from "../../components";
+
+const Home = () => {
+  return (
+    <div>
+      <PageAndSidebarWrapper>
+        <h2>Hello</h2>
+        <h4>Sure</h4>
+      </PageAndSidebarWrapper>
+    </div>
+  );
+};
+
+export default Home;
