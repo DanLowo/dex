860b063d97dc99b580610ea8e2d0ebfea359af67
DanLowo <danlowo@gmail.com>
2022-03-15 13:05:43 +0100

    setup router & Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+
+// pages
+import { Home } from "./pages/";
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
     </div>
   );
 }
