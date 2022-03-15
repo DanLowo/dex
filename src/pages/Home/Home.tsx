cfd525026ae8e60af6d7a36570512238831b98ae
DanLowo <danlowo@gmail.com>
2022-03-15 14:10:31 +0100

    home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { PageAndSidebarWrapper } from "../../components";
 
 const Home = () => {
+
+  const HomeComponent = () => (
+    <div>
+      {/*<h2>Hello</h2>*/}
+    </div>
+  )
+
   return (
     <div>
       <PageAndSidebarWrapper>
-        <h2>Hello</h2>
+        <HomeComponent />
         <h4>Sure</h4>
       </PageAndSidebarWrapper>
     </div>
