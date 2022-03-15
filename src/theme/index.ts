1ac6e9b336f05037d6ad7e46a85c049e59462c98
DanLowo <danlowo@gmail.com>
2022-03-15 13:58:23 +0100

    create theme for styled components - darkTheme

---
+++ src/theme/index.ts
@@ -0,0 +1,13 @@
+interface indexing {
+  [name: string]: object
+}
+
+const Theme: indexing = {
+  dark: {
+    backgroundDark: "#23242F",
+    backgroundLight: "#2C2D3A",
+    linkOnHover: "#0177FB"
+  }
+}
+
+export default Theme
