37bd5adf6334bbd83636ed8293fbce3385b2cde4
DanLowo <danlowo@gmail.com>
2022-03-16 17:25:52 +0100

    increase height and width of Section

--- src/components/sidebar/Section.tsx
+++ src/components/sidebar/Section.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styled from "styled-components";
 
 const StyledSection = styled.section`
-  padding: 2rem;
-  height: 70%;
+  padding: 1.2rem;
+  height: 80%;
   border-radius: 1.2rem;
-  width: calc(90% - 3rem);
+  width: calc(90% - 1.2rem);
   background-color: ${props => props.theme.backgroundTertiary};
 `
 
