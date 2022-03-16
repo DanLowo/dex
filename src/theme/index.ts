8de0e3e32ebc2717e7f5344252759ef9c8e24d8b
DanLowo <danlowo@gmail.com>
2022-03-16 14:27:19 +0100

    add more colors to theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -6,6 +6,8 @@ const Theme: indexType = {
   dark: {
     backgroundPrimary: "#23242F",
     backgroundSecondary: "#2C2D3A",
+    backgroundButton: "#3B3C4E",
+    backgroundOnline: "#53F3C3",
     linkOnHover: "#0177FB",
     textPrimary: "#fff"
   }
