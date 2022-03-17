68b63a07a45a6c8408b34efb001a2f131789d5b1
DanLowo <danlowo@gmail.com>
2022-03-17 12:16:47 +0100

    global styled components

---
+++ src/utils/GlobalStyledComponents.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const AllItemsRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: .5rem;
+`
+
+const SpaceBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export {
+  AllItemsRight, SpaceBetween
+}
