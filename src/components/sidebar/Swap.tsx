e5732e739263e383cad23f042dbe1d4f9160359a
DanLowo <danlowo@gmail.com>
2022-03-17 15:14:50 +0100

    swap component

--- src/components/sidebar/Swap.tsx
+++ src/components/sidebar/Swap.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import Section from "./Section";
+import React, { Fragment, useState, useEffect } from 'react';
+import {Section, SelectAssets, SelectField} from "../";
+
 import { AllItemsRight } from "../../utils/GlobalStyledComponents";
 import styled from "styled-components";
-import SelectField from "../inputField/SelectField";
+import {getAssetsService} from "../../services";
 
 const payDetails = {
   label: {
+    label: "pay",
     leftLabel: "Pay",
     rightLabel: "Available"
   },
@@ -14,6 +16,7 @@ const payDetails = {
 
 const receiveDetails = {
   label: {
+    label: "receive",
     leftLabel: "Receive (Estimated)",
     rightLabel: "Available"
   },
@@ -30,36 +33,83 @@ const Header = styled(AllItemsRight)`
   
   & i:hover {
     cursor: pointer;
-    
   }
 `
 
+interface getAssetsServiceProps {
+  name: string,
+  symbol: string,
+  logoURI: string
+}
 
 const Swap = () => {
+  const [allAssets, setAllAssets] = useState([{ name: "", logoURI: "", symbol: "" }])
+  const [showSelectAssets, setShowSelectAssets] = useState(false)
+
   const [pay, setPay] = useState({
-    assetName: "",
-    amount: 0
+    asset: {
+      name: "",
+      symbol: "",
+      logoURI: ""
+    },
+    amount: ""
   })
 
   const [receive, setReceive] = useState({
-    assetName: "",
-    amount: 0
+    asset: {
+      name: "",
+      symbol: "",
+      logoURI: ""
+    },
+    amount: ""
   })
+  const [selectAssetField, setSelectAssetField] = useState({currentField: pay.asset, setField: setPay})
+
+  const handleShowAssets = (type: string) => {
+    setShowSelectAssets(true)
+    const field = type === "pay" ? {currentField: pay.asset, setField: setPay} : {currentField: receive.asset, setField: setReceive}
+    setSelectAssetField(field)
+  }
+
+
+  useEffect(() => {
+    getAssetsService().then((res: Array<getAssetsServiceProps>) => {
+      setAllAssets(res)
+
+      setPay((prev: any) => ({
+        ...prev,
+        asset: res[0]
+      }))
+
+      setReceive((prev: any) => ({
+        ...prev,
+        asset: res[1]
+      }))
+
+      console.log(res)
+    })
+  }, [])
 
   return (
     <Section>
-      <Header>
-        <i className="far fa-star"/>
-        <i className="fal fa-gift"/>
-        <i className="fal fa-share-square"/>
-        <i className="fal fa-cog"/>
-      </Header>
+      {/* is select asset component is not enabled show swap component*/}
+      {showSelectAssets ? (
+        <SelectAssets assets={allAssets} assetField={selectAssetField} close={setShowSelectAssets} />
+      ) : (
+        <Fragment>
+          <Header>
+            <i className="far fa-star"/>
+            <i className="fal fa-gift"/>
+            <i className="fal fa-share-square"/>
+            <i className="fal fa-cog"/>
+          </Header>
 
-      <div>
-        <SelectField setField={setPay} fieldValue={pay.amount} label={payDetails.label} availableBalance={payDetails.availableBalance} />
-
-        <SelectField setField={setReceive} fieldValue={receive.amount} label={receiveDetails.label} availableBalance={receiveDetails.availableBalance} />
-      </div>
+          <div>
+            <SelectField currentAsset={pay.asset} handleShowAssets={handleShowAssets} setField={setPay} fieldValue={pay.amount} label={payDetails.label} availableBalance={payDetails.availableBalance} />
+            <SelectField currentAsset={receive.asset} handleShowAssets={handleShowAssets} setField={setReceive} fieldValue={receive.amount} label={receiveDetails.label} availableBalance={receiveDetails.availableBalance} />
+          </div>
+        </Fragment>
+      )}
     </Section>
   );
 };
