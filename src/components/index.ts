92cc705bf4642d9f278db70ca4905b3e5cb190a1
DanLowo <danlowo@gmail.com>
2022-03-17 15:12:57 +0100

    add components to index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,11 @@ import PageAndSidebarWrapper from "./pageAndSidebarWrapper/PageAndSidebarWrapper
 import NavigationBar from "./navigation/NavigationBar";
 import SideBarNavigation from "./navigation/SideBarNavigation";
 import Swap from "./sidebar/Swap"
+import Section from "./sidebar/Section";
+import SelectField from "./inputField/SelectField";
+import SelectAssets from "./inputField/SelectAssets";
+
 
 export {
-  PageAndSidebarWrapper, NavigationBar, SideBarNavigation, Swap
+  PageAndSidebarWrapper, NavigationBar, SideBarNavigation, Swap, Section, SelectField, SelectAssets
 }
