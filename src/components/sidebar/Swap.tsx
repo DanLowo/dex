e3a04fadfa0a82016a8b0519f891bc6d07f14454
DanLowo <danlowo@gmail.com>
2022-03-18 12:10:29 +0100

    conversion & switch design

--- src/components/sidebar/Swap.tsx
+++ src/components/sidebar/Swap.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Section, SelectAssets, SelectField } from "../";
-import { AllItemsRight } from "../../utils/GlobalStyledComponents";
+import {AllItemsRight, Button, CenterItems, Line} from "../../utils/GlobalStyledComponents";
 import styled from "styled-components";
 import { swapUseEffect } from "../../services/useEffectServices";
 
@@ -23,6 +23,7 @@ const receiveDetails = {
 }
 
 const Header = styled(AllItemsRight)`
+  margin-bottom: 1.5rem;
   & i {
     padding: .6rem;
     border-radius: .5rem;
@@ -32,6 +33,28 @@ const Header = styled(AllItemsRight)`
   
   & i:hover {
     cursor: pointer;
+  }
+`
+
+const SwitchAssetButtonIcon = styled(CenterItems)`
+  cursor: pointer;
+  margin-top: 1.2rem;
+  & i {
+    font-size: 1.3rem;
+    color: ${props => props.theme.textPrimary};
+  }
+`
+
+const ConversionRate = styled(CenterItems)`
+  margin: 1.3rem 0 .2rem 0;
+  font-size: .85rem;
+  font-weight: bold;
+  letter-spacing: .03rem;
+  color: ${props => props.theme.textSecondary};
+  
+  & i {
+    cursor: pointer;
+    color: ${props => props.theme.textPrimary};
   }
 `
 
@@ -87,8 +110,19 @@ const Swap = () => {
 
           <div>
             <SelectField currentAsset={pay.asset} handleShowAssets={handleShowAssets} setField={setPay} fieldValue={pay.amount} label={payDetails.label} availableBalance={payDetails.availableBalance} />
+            <SwitchAssetButtonIcon>
+              <i className="fal fa-arrow-circle-down" />
+            </SwitchAssetButtonIcon>
             <SelectField currentAsset={receive.asset} handleShowAssets={handleShowAssets} setField={setReceive} fieldValue={receive.amount} label={receiveDetails.label} availableBalance={receiveDetails.availableBalance} />
           </div>
+
+          <ConversionRate>
+            <p>1 {pay.asset.symbol} = 1 {receive.asset.symbol}</p>
+            <i className="fal fa-sync" />
+          </ConversionRate>
+
+          <Button disabled={true}>Confirm Order</Button>
+          <Line/>
         </Fragment>
       )}
     </Section>
