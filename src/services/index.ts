9a694c3a6699e265c5d14f63ea31c706affe245c
DanLowo <danlowo@gmail.com>
2022-03-19 12:29:45 +0100

    display all assets

--- src/services/index.ts
+++ src/services/index.ts
@@ -10,7 +10,6 @@ interface getAssetsServiceProps {
 export const getAssetsService = async (): Promise<Array<getAssetsServiceProps>> => {
   try {
     const { data: { tokens } } = await axios.get("https://tokens.pancakeswap.finance/pancakeswap-extended.json")
-    tokens.length = 100
 
     return tokens.map(({name, symbol, logoURI, address}: getAssetsServiceProps) => ({ name, symbol, logoURI, address}))
 
