a9186c8aeaff67e9548f3939676840be195b8bf0
DanLowo <danlowo@gmail.com>
2022-03-19 14:46:27 +0100

    export all icons

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import BSCIcon from "./bsc.png"
+import MetamaskIcon from "./metamask.png"
+import TrustWalletIcon from "./twt.png"
+import WalletConnectIcon from "./wc.png"
+
+
+export {
+  BSCIcon, MetamaskIcon, TrustWalletIcon, WalletConnectIcon
+}
